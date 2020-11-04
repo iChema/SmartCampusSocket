61648211d7a33c047612a1ac51b51f1cd6cf074f
chema <chema@chema.com>
2020-11-04 05:06:18 -0600

    agregar get mac address

--- socket.js
+++ socket.js
@@ -7,6 +7,7 @@ const client = new MongoClient(uri);
 var http = require('http');
 var server = http.createServer();
 var io = require('socket.io').listen(server);
+var databaseName = process.env.DATABASE_NAME
 var curp = {};
 
 class Users {
@@ -76,6 +77,10 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('users', Object.keys(curp))
     }
 
+    socket.on('getAgentsOnline', ()=>{
+        getAgentsOnlineDB();
+    });
+
     socket.on('disconnect', () => {
         let user = users.getUser(socket.id);
 
@@ -92,11 +97,49 @@ io.sockets.on('connection', function (socket) {
 server.listen(process.env.PORT, function(){
     console.log('Servidor de sockets activo');
 
-    testDB();
+    getAgentsOnlineDB();
 });
 
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
+}
+
+function getMacAddressList(agentsOnline) {
+    list = [];
+
+    agentsOnline.forEach(agentx => {
+        mac = agentx['_id'];
+        list.push(mac.toString( 16 ).match( /.{1,2}/g ).reverse().join( ':' ) );
+    });
+
+    return list;
+}
+
+async function getAgentsOnlineDB() {
+    try {
+        await client.connect();
+
+        const database = client.db(databaseName);
+        const collection = database.collection("agent");
+    
+        // Query for a movie that has the title 'The Room'
+        const query = { status : "online" };
+     
+        const options = {};
+    
+        const agents = await collection.find(query, options)
+        .toArray()
+        .then(agentsOnline => {
+            //return agentsOnline
+            io.sockets.emit('setAgentOnline', getMacAddressList(agentsOnline));
+            console.log(getMacAddressList(agentsOnline));
+        });
+    } catch(e) {
+        console.log(e);
+        await client.close();
+    }finally {
+        await client.close();
+    }
 }
 
 async function testDB() {
@@ -115,7 +158,7 @@ async function testDB() {
     
         // since this method returns the matched document, not a cursor, print it directly
         console.log(agent);
-      } finally {
+    } finally {
         await client.close();
-      }
+    }
 }
