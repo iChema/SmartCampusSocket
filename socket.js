c82a4865fb628e3500eda5d53870abde0b8c5c28
chema <chema@chema.com>
2020-11-09 07:03:29 -0600

    update user and agents

--- socket.js
+++ socket.js
@@ -5,139 +5,180 @@ dotenv.config();
 const uri = process.env.URI;
 var http = require('http');
 const { get } = require("https");
+const { type } = require("os");
 var server = http.createServer();
 var io = require('socket.io').listen(server);
-var databaseName = process.env.DATABASE_NAME
-var curp = {};
+var databaseName = process.env.DATABASE_NAME;
+var users = [];
+var agents = [];
 
-class Users {
-    constructor() {
-        this.users = [];
+// A class that describes the attributes 
+// that each socket user has
+class User {
+    constructor(socketId,id,type) {
+        this.socketId = socketId;
+        this.id = id;
+        this.type = type;
     }
-
-    addUser(id, curp, sala) {
-        let user = { id, curp, sala };
-        this.users.push(user);
-        return user;
-    }
-
-    getUserList() {
-        let list = [];
-        this.users.forEach(user => list.push(user.curp));
-
-        return list;
-    }
-
-    getUser(id) {
-        return this.users.filter((user) => user.id === id)[0];
-    }
-
-    getUser2(curp) {
-        return this.users.filter((user) => user.curp === curp)[0];
-    }
-
-    removeUser(curp) {
-        let user = this.getUser2(curp);
-
-        if (user) {
-            this.users = this.users.filter((user) => user.curp !== curp);
-        }
-
-        return user;
-    }
-
 }
 
-var users = new Users();
-var agents = new Users();
+
+// Functions to manage the list of users and agents
+function addUser(socketId, id, type) {
+    let user = new User(socketId,id,type);
+    this.users.push(user);
+    return user;
+}
+
+function addAgent(socketId, id, type) {
+    let agent = new User(socketId,id,type);
+    this.agents.push(agent);
+    return agent;
+}
+
+function getUserBySocketId(id) {
+    return this.users.filter((user) => user.socketId === id)[0];
+}
+
+function getAgentBySocketId(id) {
+    return this.agents.filter((user) => user.socketId === id)[0];
+}
+
+function getUserById(id) {
+    return this.users.filter((user) => user.id === id)[0];
+}
+
+function getAgentById(id) {
+    return this.agents.filter((user) => user.id === id)[0];
+}
+
+function removeUser(id) {
+    let user = getUserById(id);
+
+    if (user) {
+        this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+}
+
+function removeAgent(id) {
+    let user = getAgentById(id);
+
+    if (user) {
+        this.agents = this.agents.filter((user) => user.id !== id);
+    }
+
+    return user;
+}
 
 io.sockets.on('connection', function (socket) {    
-    //console.log('New User');
-    socket.on('join', (userID, callback) => {
-        console.log('Usuarios conectados');
-        users.removeUser(userID.curp);
-        socket.join(userID.sala);
-        users.addUser(socket.id, userID.curp, userID.sala);
-        console.log(users.getUserList());
+
+    // When human user join to the socket service
+    socket.on('join', (user) => {
+        removeUser(user.id);
+        socket.join(user.type);
+        addUser(socket.id, user.id, user.type);
         getAgentsOnlineDB(socket.id);
-        callback();
     });
 
-    socket.on('here',(macAgent)=> {
+    // Update the position of the user's match with an agent
+    socket.on('here',(macAddressAgent)=> {
         try{
-            agentId = int(macAgent.replace(':', ''), 16)
-            user = users.getUser(socket.id);
-            updateUserPosition(agentId,user.curp);
+            agentId = getIdFromMac(macAddressAgent);
+            user = getUserBySocketId(socket.id);
+            updateUserPosition(agentId,user.id);
         } catch (e) {}
     });
 
+    // When agent user join to the socket service
     socket.on('joinAgent', (agentId) => {
-        console.log('Agentes conectados');
-        agents.removeUser(agentId);
-        agents.addUser(socket.id, agentId, 0);
-        console.log(agents.getUserList());
+        removeAgent(agentId);
+        addAgent(socket.id, agentId, 0);
         setAgentOnline(agentId,true);
     });
 
-    socket.on('message', function (msg) {
-        let user = users.getUser(socket.id);
-
-        if (user) {
-            io.sockets.to(user.sala).emit('message', msg);
-        }
-    });
-
-    function updateUsers() {
-        io.sockets.emit('users', Object.keys(curp))
-    }
-
+    // Get the list of the agents online
     socket.on('getAgentsOnline', ()=>{
         getAgentsOnlineDB(socket.id);
     });
 
-    socket.on('disconnect', () => {
-        let user = users.getUser(socket.id);
+    // Send a broadcast message by the type
+    socket.on('message', function (msg) {
+        let user = getUserBySocketId(socket.id);
 
         if (user) {
-            users.removeUser(user.curp);
-            console.log('Socket disconnected: ' + user.curp);
-            console.log('Usuarios conectados');
-            console.log(users.getUserList());
+            io.sockets.to(user.type).emit('message', msg);
+        }
+    });
+
+    // Triggered when a user disconect
+    socket.on('disconnect', () => {
+        let user = getUserBySocketId(socket.id);
+
+        if (user) {
+            users.removeUser(user.id);
         } else {
-            let agent = agents.getUser(socket.id);
+            let agent = getAgentBySocketId(socket.id);
+
             if(agent) {
-                agents.removeUser(agent.curp);
-                console.log('Agente disconnected: ' + agent.curp);
-                console.log('Agentes conectados');
-                console.log(agents.getUserList());
+                agents.removeUser(agent.id);
+                setAgentStatus(agent.id,false);
             }
         }
     })
 });
 
-server.listen(process.env.PORT, function(){
-    console.log('Servidor de sockets activo');
-
-    testDB();
-    //getAgentsOnlineDB();
-});
-
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
+//--------------------HELPERS-----------------------
+// Change de Mac Address of the device for an identifier 
+function getIdFromMac(mac){
+    return int(mac.replace(':', ''), 16);
 }
 
-function getMacAddressList(agentsOnline) {
-    list = [];
+// Change de identifier of the agent for the mac address  
+function getMacFromID(id) {
+    return id.toString( 16 ).match( /.{1,2}/g ).reverse().join( ':' )
+}
 
-    agentsOnline.forEach(agentx => {
-        mac = agentx['_id'];
-        list.push(mac.toString( 16 ).match( /.{1,2}/g ).reverse().join( ':' ) );
+// Gets the identifications of the agent list
+function getMacAddressList(agentsOnline) {
+    let list = [];
+
+    agentsOnline.forEach(agent => {
+        id = agent['_id'];
+        list.push(getMacFromID(id));
     });
 
     return list;
 }
 
-async function setAgentOnline(agentId,bool) {
+//-----------------DATABASE-------------------
+// Gets the list of the agents online in database
+async function getAgentsOnlineDB(id) {
+    
+    var client = new MongoClient(uri);
+
+    await client.connect().then((client)=> {
+        const database = client.db(databaseName);
+        const collection = database.collection("agent");
+    
+        // Query for a agents that has the status 'online'
+        const query = { status : "online" };
+     
+        const options = {};
+    
+        collection.find(query, options).toArray(function(err, result) {
+            if (err) throw err;
+            io.sockets.to(id).emit('setAgentOnline', getMacAddressList(result));
+            client.close();
+          });
+    }).catch((e)=>{
+        console.log(e);
+    });
+}
+
+// Update agent status in database when connecting or disconnecting
+async function setAgentStatus(agentId,bool) {
     var client = new MongoClient(uri);
     let status =  (bool) ? 'online' : 'offline'
 
@@ -150,7 +191,7 @@ async function setAgentOnline(agentId,bool) {
      
         const options = {};
     
-        collection.updateOne(query, { 'status' :  } ,options).toArray(function(err, result) {
+        collection.updateOne(query, { 'status' : status } ,options).toArray(function(err, result) {
             if (err) throw err;
             client.close();
           });
@@ -159,6 +200,7 @@ async function setAgentOnline(agentId,bool) {
     });
 }
 
+// Update agent status in database when connecting or disconnecting
 async function updateUserPosition(agentId,userId){
     var client = new MongoClient(uri);
 
@@ -178,51 +220,7 @@ async function updateUserPosition(agentId,userId){
     });
 }
 
-async function getAgentsOnlineDB(id) {
-    
-    var client = new MongoClient(uri);
-
-    await client.connect().then((client)=> {
-        const database = client.db(databaseName);
-        const collection = database.collection("agent");
-    
-        // Query for a agents that has the status 'online'
-        const query = { status : "online" };
-     
-        const options = {};
-    
-        collection.find(query, options).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            console.log(getMacAddressList(result));
-            io.sockets.to(id).emit('setAgentOnline', getMacAddressList(result));
-            client.close();
-          });
-    }).catch((e)=>{
-        console.log(e);
-    });
-}
-
-async function testDB() {
-    try {
-        var client = new MongoClient(uri);
-
-        await client.connect();
-
-        const database = client.db("smart_campus");
-        const collection = database.collection("agent");
-    
-        // Query for a movie that has the title 'The Room'
-        const query = {status : "online"};
-    
-        const options = {};
-    
-        const agent = await collection.find(query, options).toArray()
-        .then(agentsOnline => {
-            console.log(getMacAddressList(agentsOnline));
-        });
-    
-    } finally {
-        await client.close();
-    }
-}
+// Begin the socket service
+server.listen(process.env.PORT, function(){
+    console.log('Servidor de sockets activo');
+});
